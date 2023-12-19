63100ccd76f93d2f07ea8eb62ae560c83b8c746e
Zhaoyu Pan <panzhaoyu11@gmail.com>
2023-12-19 22:10:32 +1100

    initial commit

---
+++ src/main/java/com/zhaoyu/reggie/ReggieApplication.java
@@ -0,0 +1,13 @@
+package com.zhaoyu.reggie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReggieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class, args);
+    }
+
+}
