80a72c1bb5638064c0ee24b2a52e5281f065b5ce
Zhaoyu Pan <panzhaoyu11@gmail.com>
2023-12-22 22:33:42 +1100

    implement user login filter

---
+++ src/main/java/com/zhaoyu/easyeats/filter/loginCheckFilter.java
@@ -0,0 +1,67 @@
+package com.zhaoyu.easyeats.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.zhaoyu.easyeats.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
+@Slf4j
+public class loginCheckFilter implements Filter {
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestURI = request.getRequestURI();
+
+        log.info("filtered request: {}", requestURI);
+
+        request.getRequestURI();
+        String[] urls = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**"
+        };
+
+        boolean check = check(urls,requestURI);
+
+        if(check){
+            log.info("this request{} does not need to handle", requestURI);
+            filterChain.doFilter(request,response);
+            return;
+        }
+
+
+        if(request.getSession().getAttribute("employee")!= null){
+            log.info("user logged in, user id:{}", request.getSession().getAttribute("employee"));
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        response.getWriter().write(JSON.toJSONString(R.error("not login")));
+        return;
+
+
+    }
+
+    public boolean check(String[] urls, String requestURI){
+        for(String url : urls){
+            boolean match = PATH_MATCHER.match(url, requestURI);
+            if(match){
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
