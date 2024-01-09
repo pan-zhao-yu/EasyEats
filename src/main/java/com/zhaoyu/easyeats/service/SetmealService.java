1b11f38b0f22b2aeb180913454fcdf7aeeecb76f
Zhaoyu Pan <panzhaoyu11@gmail.com>
2024-01-09 22:19:35 +1100

    update setmeal status, 0(not available)/1(available)

--- src/main/java/com/zhaoyu/easyeats/service/SetmealService.java
+++ src/main/java/com/zhaoyu/easyeats/service/SetmealService.java
@@ -1,7 +1,17 @@
 package com.zhaoyu.easyeats.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhaoyu.easyeats.dto.SetmealDto;
 import com.zhaoyu.easyeats.entity.Setmeal;
 
+import java.util.List;
+
 public interface SetmealService extends IService<Setmeal> {
+
+
+    public void saveWithDish(SetmealDto setmealDto);
+
+    public void removeWithDish(List<Long> ids);
+
+//    public void updateSetmealStatus(Integer status, List<Long> ids);
 }
