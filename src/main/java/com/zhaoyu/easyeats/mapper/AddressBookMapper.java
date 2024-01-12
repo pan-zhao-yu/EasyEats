e3b29b7f89700ba1a1af28f8132d75bf4a1cd19a
Zhaoyu Pan <panzhaoyu11@gmail.com>
2024-01-12 23:24:14 +1100

    create, update, search user AddressBook

---
+++ src/main/java/com/zhaoyu/easyeats/mapper/AddressBookMapper.java
@@ -0,0 +1,10 @@
+package com.zhaoyu.easyeats.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zhaoyu.easyeats.entity.AddressBook;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AddressBookMapper extends BaseMapper<AddressBook> {
+
+}
---
+++ src/main/java/com/zhaoyu/easyeats/service/AddressBookService.java
@@ -0,0 +1,8 @@
+package com.zhaoyu.easyeats.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.zhaoyu.easyeats.entity.AddressBook;
+
+public interface AddressBookService extends IService<AddressBook> {
+
+}
